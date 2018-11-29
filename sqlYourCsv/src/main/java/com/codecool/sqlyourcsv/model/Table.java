fbbe2a7891bda77087da1b35ef54b78be5c9735d
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-11-29 13:34:11 +0100

    Add Table model

---
+++ sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/model/Table.java
@@ -0,0 +1,46 @@
+package com.codecool.sqlyourcsv.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Table {
+
+    private Entry headers;
+    private List<Entry> tableContent;
+
+    public Table(Entry headers, List<Entry> content) {
+        this.headers = headers;
+        this.tableContent = content;
+    }
+
+    public List<String> get(String s) {
+        // TODO: 29/11/2018 Implement
+        return null;
+    }
+
+    private int findColumnIndex(String s) {
+        String column = Stream.of(this.headers.getContent())
+            .filter(w -> w.equalsIgnoreCase(s))
+            .findFirst()
+            .orElse("empty result");
+
+        return Arrays.asList(this.headers.getContent()).indexOf(column);
+    }
+
+    public Entry getHeaders() {
+        return this.headers;
+    }
+
+    public void setHeaders(Entry headers) {
+        this.headers = headers;
+    }
+
+    public List<Entry> getTableContent() {
+        return this.tableContent;
+    }
+
+    public void setTableContent(List<Entry> tableContent) {
+        this.tableContent = tableContent;
+    }
+}
