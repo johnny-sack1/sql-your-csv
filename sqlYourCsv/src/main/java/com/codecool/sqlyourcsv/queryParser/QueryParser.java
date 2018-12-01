0cb1104de55ea88c499b5fff250a4b40f910ec49
johnny-sack1 <jonymaka@gmail.com>
2018-12-01 11:39:41 +0100

    Relieve QueryParser of handling exceptions and checking if file exists

--- sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/queryParser/QueryParser.java
+++ sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/queryParser/QueryParser.java
@@ -1,47 +1,36 @@
 package com.codecool.sqlyourcsv.queryParser;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class QueryParser {
 
-    public void parse(String query) throws FileNotFoundException {
-        try {
-            validate(query);
-        } catch (InvalidQueryException e) {
-            e.printStackTrace();
-        }
-        
-        String fileName = getFileName(query);
-
-        if (!csvExists(fileName)) {
-            throw new FileNotFoundException();
-        }
-
-
-    }
-
-    private void validate(String query) throws InvalidQueryException {
-        String regex = "(?i:SELECT \\S+ FROM \\w+\\.csv( WHERE \\w+)?;)";
+    public void validate(String query) throws InvalidQueryException {
+        String regex = "(?i:SELECT .+ FROM \\w+\\.csv( WHERE \\w+ (<|>|=|<>) \\d+)?;)";
 
         if (!query.matches(regex)) {
             throw new InvalidQueryException();
         }
     }
 
-    private boolean csvExists(String fileName) {
-        File f = new File(fileName);
-
-        return f.exists() && !f.isDirectory();
-    }
-
-    private String getFileName(String query) {
+    public String getFileName(String query) {
         String regex = "(\\w+?)\\.csv";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(query);
         matcher.find();
         return matcher.group(1);
+    }
+
+    public String[] getColsToDisplay(String query) {
+        String regex = "SELECT (.+) FROM .+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(query);
+        matcher.find();
+        return matcher.group(1).split(", ");
+    }
+
+    public boolean hasWhereClause(String query) {
+        String regex = "SELECT .+ FROM .+ WHERE .+";
+        return query.matches(regex);
     }
 }
