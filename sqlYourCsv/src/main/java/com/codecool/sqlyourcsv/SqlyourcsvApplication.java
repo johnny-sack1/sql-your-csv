fc272acae9e056d0d4f471a9f83cfad229c85384
johnny-sack1 <jonymaka@gmail.com>
2018-12-04 20:28:40 +0100

    Add sample csv file and test code

--- sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/SqlyourcsvApplication.java
+++ sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/SqlyourcsvApplication.java
@@ -1,12 +1,13 @@
 package com.codecool.sqlyourcsv;
 
-import org.springframework.boot.SpringApplication;
+import com.codecool.sqlyourcsv.dataProvider.DataProvider;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SqlyourcsvApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqlyourcsvApplication.class, args);
+		DataProvider provider = new DataProvider();
+		System.out.println(provider.query("SELECT * FROM 50contacts.csv;"));
 	}
 }
