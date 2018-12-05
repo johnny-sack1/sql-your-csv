cf20bd41da5696a66e55ec5e769d77fcb530dcbd
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-12-05 02:39:16 +0100

    Update pom & implement WebController

--- sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/controller/WebController.java
+++ sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/controller/WebController.java
@@ -1,4 +1,22 @@
 package com.codecool.sqlyourcsv.controller;
 
+import com.codecool.sqlyourcsv.dataProvider.IDataProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class WebController {
+
+    private IDataProvider iDataProvider;
+
+    @Autowired
+    public WebController(IDataProvider iDataProvider) {
+        this.iDataProvider = iDataProvider;
+    }
+
+
+    public String doGet() {
+        return "index";
+    }
 }
+
