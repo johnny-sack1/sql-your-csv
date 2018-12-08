d7fc12c9f20136d6a3d7d39e4fecfe365e64efb0
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-12-08 16:35:41 +0100

    Refactor WebController
    Hardcoded query for now

--- sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/controller/WebController.java
+++ sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/controller/WebController.java
@@ -1,10 +1,14 @@
 package com.codecool.sqlyourcsv.controller;
 
 import com.codecool.sqlyourcsv.dataProvider.IDataProvider;
+import com.codecool.sqlyourcsv.model.Table;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,17 +22,11 @@ public class WebController {
         this.iDataProvider = iDataProvider;
     }
 
-
     @GetMapping("/")
-    public String doGet() {
+    public String doGet(Model model) throws Exception {
+        Table table = this.iDataProvider.query("Sheet1");
+        model.addAttribute("table", table);
         return "index";
-    }
-
-    @PostMapping("/")
-    @ResponseBody
-    public String doPost(@RequestParam("query") String query) {
-        return this.iDataProvider.query(query);
-        //return "index";
     }
 }
 
