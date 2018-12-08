d80884f1d25c396ea1ac70d80f466eef816edbd7
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-12-08 16:32:14 +0100

    Move GoogleAuthUtil class to util package

--- sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/util/GoogleAuthorizeUtil.java
+++ sqlYourCsv/src/main/java/com/codecool/sqlyourcsv/util/GoogleAuthorizeUtil.java
@@ -1,4 +1,4 @@
-package com.codecool.sqlyourcsv.dataProvider;
+package com.codecool.sqlyourcsv.util;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
